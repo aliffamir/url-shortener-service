af768a33a6d16d27434b184850cc01686b353653
Aliff Amir <aliff6406@gmail.com>
2025-07-08 20:06:02 +0100

    add extensions for infrastructure DI

---
+++ UrlShortener.Infrastructure/Extensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+using UrlShortener.Core.Interfaces;
+using UrlShortener.Infrastructure.Data;
+using UrlShortener.Infrastructure.Repositories;
+
+namespace UrlShortener.Infrastructure;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+             options.UseNpgsql(configuration.GetConnectionString("PostgresConnection"))
+        )
+        .AddScoped<IShortUrlRepository, EfShortUrlRepository>();
+
+        return services;
+    }
+}
