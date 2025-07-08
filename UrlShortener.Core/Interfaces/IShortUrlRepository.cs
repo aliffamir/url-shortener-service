df86e3915b7fd0f688e5de5cf2867dda71e3692a
Aliff Amir <aliff6406@gmail.com>
2025-07-08 19:29:57 +0100

    add short url repository interface

--- UrlShortener.Core/Interfaces/IShortUrlRepository.cs
+++ UrlShortener.Core/Interfaces/IShortUrlRepository.cs
@@ -1,5 +1,9 @@
+using UrlShortener.Core.Entities;
+
 namespace UrlShortener.Core.Interfaces;
 
 public interface IShortUrlRepository
 {
+    Task<ShortenedUrl?> CreateAsync(ShortenedUrl shortenedUrl);
+    Task<ShortenedUrl?> GetByKeyAsync(string key); 
 }
