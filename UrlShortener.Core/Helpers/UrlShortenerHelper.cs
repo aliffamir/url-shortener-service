404ec278556b35ac65a34359b31860d4e4a5ed3f
Aliff Amir <aliff6406@gmail.com>
2025-07-14 15:23:38 +0100

    add helper to encode/decode short key

---
+++ UrlShortener.Core/Helpers/UrlShortenerHelper.cs
@@ -0,0 +1,29 @@
+namespace UrlShortener.Core.Helpers;
+
+public class UrlShortenerHelper {
+    private static readonly string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static readonly int Base = Alphabet.Length;
+
+    public static string Encode(int i) {
+        if (i == 0) return Alphabet[0].ToString();
+
+        var s = string.Empty;
+
+        while (i > 0) {
+            s += Alphabet[i % Base];
+            i /= Base;
+        }
+
+        return string.Join(string.Empty, s.Reverse());
+    }
+
+    public static int Decode(string s) {
+        var i = 0;
+
+        foreach (var c in s) {
+            i = (i * Base) + Alphabet.IndexOf(c);
+        }
+
+        return i;
+    }
+}
