650e21266f9106f8a47a13200af915c8c544e556
Aliff Amir <aliff6406@gmail.com>
2025-07-14 19:17:05 +0100

    add Core dependency

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -1,12 +1,16 @@
 using Scalar.AspNetCore;
 
+using UrlShortener.Api.Endpoints;
+using UrlShortener.Core;
 using UrlShortener.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services
+    .AddInfrastructure(builder.Configuration)
+    .AddCore();
 
 var app = builder.Build();
 
@@ -15,5 +19,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+
+app.MapUrlShortenerEndpoints();
 
 app.Run();
