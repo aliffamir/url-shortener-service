8d9110aec88d78e1f001a4abcd74e9e44c29c521
Aliff Amir <aliff6406@gmail.com>
2025-07-19 10:29:22 +0100

    use DateTimeoffset

--- UrlShortener.Contracts/Dtos/ShortUrlResponse.cs
+++ UrlShortener.Contracts/Dtos/ShortUrlResponse.cs
@@ -4,5 +4,5 @@ public record class ShortUrlResponse(
         string LongUrl,
         string ShortUrl,
         string Key,
-        DateTime CreatedAt,
-        DateTime ExpiresAt);
+        DateTimeOffset CreatedAt,
+        DateTimeOffset ExpiresAt);
